af089563716aeeab242526e31fa0a6f8e6dd91fa
rahulraj0402 <rahul3004rt@gmail.com>
2024-10-29 02:27:03 +0530

    first commit

---
+++ src/main/java/com/rahul/optimisedApi/OptimisedApiApplication.java
@@ -0,0 +1,13 @@
+package com.rahul.optimisedApi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OptimisedApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OptimisedApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rahul/optimisedApi/OptimisedApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rahul.optimisedApi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OptimisedApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
