fed3a65c9f840d02c03eba5c67a5086dd0040a8b
rahulraj0402 <rahul3004rt@gmail.com>
2024-11-09 03:40:24 +0530

    implemented conreoller and all the crud rest API's with mysql is working fine

---
+++ src/main/java/com/rahul/optimisedApi/controller/NotesController.java
@@ -0,0 +1,36 @@
+package com.rahul.optimisedApi.controller;
+
+import com.rahul.optimisedApi.entity.Notes;
+import com.rahul.optimisedApi.services.NotesService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/v1/notes")
+public class NotesController {
+
+    @Autowired
+    private NotesService notesService;
+
+    // create
+    @PostMapping
+    public ResponseEntity<Notes> create(@RequestBody Notes notes){
+        return ResponseEntity.status(HttpStatus.CREATED).body(notesService.create(notes));
+    }
+
+    // get notes
+    @GetMapping
+    public ResponseEntity<List<Notes>> getAll(){
+        return ResponseEntity.status(HttpStatus.OK).body(notesService.getAll());
+    }
+
+    @GetMapping("/{notesId}")
+    public ResponseEntity<Notes> getSingle(@PathVariable String notesId){
+        return ResponseEntity.ok(notesService.getById(notesId));
+    }
+
+}
--- src/main/java/com/rahul/optimisedApi/services/NotesService.java
+++ src/main/java/com/rahul/optimisedApi/services/NotesService.java
@@ -1,8 +1,12 @@
 package com.rahul.optimisedApi.services;
 
+import com.rahul.optimisedApi.entity.Notes;
 import com.rahul.optimisedApi.repository.NotesRepo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.UUID;
 
 @Service
 public class NotesService {
@@ -11,12 +15,42 @@ public class NotesService {
     private NotesRepo notesRepo;
 
     // save
+    public Notes create(Notes notes){
+        notes.setId(UUID.randomUUID().toString());
+        return notesRepo.save(notes);
+    }
 
     // get
+    public List<Notes> getAll(){
+        return notesRepo.findAll();
+    }
+
+//     get by ID
+    public Notes getById(String noteId){
+        return notesRepo.findById(noteId).orElseThrow(() ->
+                new RuntimeException("note with id : " + noteId + " is not found "));
+    }
+
 
     // update
+    public Notes updateNote(String noteId , Notes notes){
+        Notes notes1 = notesRepo.findById(noteId).orElseThrow(() ->
+                new RuntimeException("note with id : " + noteId + " is not found "));
+
+        notes1.setTitle(notes.getTitle());
+        notes1.setContent(notes.getContent());
+        notes1.setLive(notes.isLive());
+
+        return notesRepo.save(notes1);
+    }
 
     // delete
+    public void delete(String noteId){
+        Notes notes1 = notesRepo.findById(noteId).orElseThrow(() ->
+                new RuntimeException("note with id : " + noteId + " is not found "));
+
+        notesRepo.delete(notes1);
+    }
 
 
 }
